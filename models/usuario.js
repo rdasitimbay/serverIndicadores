1c1b0109dc84af923f0f1a761c0e0ad2e164f7a8
capo2786 <capo2786@gmail.com>
2021-03-03 14:13:49 -0500

    cambios de servidor

--- models/usuario.js
+++ models/usuario.js
@@ -3,21 +3,21 @@ const mongoose = require('mongoose');
 
 
 var rolesValidos = {
-   values: ['ADMIN_ROLE', 'USER_ROLE','RECTOR_ROLE','VICERECTOR_ROLE','DIRECTOR_ROLE','PLANIFICADOR_ROLE','UPDI_ROLE',  'INACTIVO'],
+   values: ['ADMIN_ROLE', 'USER_ROLE', 'RECTOR_ROLE', 'VICERECTOR_ROLE', 'DIRECTOR_ROLE', 'PLANIFICADOR_ROLE', 'UPDI_ROLE', 'INACTIVO'],
    message: '{VALUE} no es un rol permitido'
 };
 
 
 const UsuarioSchema = mongoose.Schema({
 
-  nombre: { type: String },
+   nombre: { type: String },
    email: { type: String, required: true, unique: true },
    password: { type: String, required: true, },
    img: { type: String },
    role: {
       type: String,
-       required: true,
-        default: 'USER_ROLE',
+      required: true,
+      default: 'USER_ROLE',
       enum: rolesValidos
    },
 
